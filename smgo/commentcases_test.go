2b2aa88c0f71e405ae5558fd9b89966f0bc489cd
Jorge Riquelme <jorge@larix.cl>
2018-04-20 13:12:57 -0300

    Test case for imports with comments.

--- smgo/commentcases_test.go
+++ smgo/commentcases_test.go
@@ -72,6 +72,69 @@ func TestParseCommentCases(t *testing.T) {
 				ParsingErrors: nil,
 			},
 		},
+		{
+			Src: "comment_import.go_src",
+			ExpectedFile: &smgo.File{
+				LocationSpan: newLocationSpan(1, 0, 21, 17),
+				FooterSpan:   smgo.RuneSpan{0, -1},
+				Children: []smgo.Node{
+					&smgo.Terminal{
+						Type:         smgo.PackageNode,
+						Name:         "commentimport",
+						LocationSpan: newLocationSpan(1, 0, 1, 22),
+						Span:         smgo.RuneSpan{0, 21},
+					},
+					&smgo.Container{
+						Type:         smgo.ImportNode,
+						Name:         "import",
+						LocationSpan: newLocationSpan(2, 0, 13, 2),
+						HeaderSpan:   smgo.RuneSpan{22, 63},
+						FooterSpan:   smgo.RuneSpan{147, 148},
+						Children: []smgo.Node{
+							&smgo.Terminal{
+								Type:         smgo.ImportNode,
+								Name:         "io",
+								LocationSpan: newLocationSpan(6, 0, 7, 6),
+								Span:         smgo.RuneSpan{64, 76},
+							},
+							&smgo.Terminal{
+								Type:         smgo.ImportNode,
+								Name:         "io/ioutil",
+								LocationSpan: newLocationSpan(8, 0, 8, 13),
+								Span:         smgo.RuneSpan{77, 89},
+							},
+							&smgo.Terminal{
+								Type:         smgo.ImportNode,
+								Name:         "github.com/pkg/errors",
+								LocationSpan: newLocationSpan(9, 0, 12, 25),
+								Span:         smgo.RuneSpan{90, 146},
+							},
+						},
+					},
+					&smgo.Container{
+						Type:         smgo.ImportNode,
+						Name:         "import",
+						LocationSpan: newLocationSpan(14, 0, 16, 10),
+						HeaderSpan:   smgo.RuneSpan{149, 179},
+						FooterSpan:   smgo.RuneSpan{180, 181},
+						Children:     nil,
+					},
+					&smgo.Terminal{
+						Type:         smgo.ImportNode,
+						Name:         "fmt",
+						LocationSpan: newLocationSpan(17, 0, 18, 13),
+						Span:         smgo.RuneSpan{182, 195},
+					},
+					&smgo.Terminal{
+						Type:         smgo.ImportNode,
+						Name:         "strings",
+						LocationSpan: newLocationSpan(19, 0, 21, 17),
+						Span:         smgo.RuneSpan{196, 228},
+					},
+				},
+				ParsingErrors: nil,
+			},
+		},
 	}
 	for _, testCase := range cases {
 		name := testCase.Src[len("comment_"):strings.LastIndex(testCase.Src, ".")]
